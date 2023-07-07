853fbfca3fa56854bfa5a66cc08a7fbff902f923
serna37 <serna37@serna37noMacBook-Pro.local>
2023-07-07 13:22:07 +0900

    wip

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,14 +3,16 @@ import Signin from '../components/Signin'
 import Tabs from '../components/Tabs'
 
 export default function Router() {
+  // <BrowserRouter basename={process.env.PUBLIC_URL}>
+  // <BrowserRouter basename='/'>
   return (
     <div>
-      <BrowserRouter basename='/'>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path='/' element={<Signin />}></Route>
           <Route path='/notes' element={<Tabs />}></Route>
         </Routes>
       </BrowserRouter>
-    </div>
+    </div >
   )
 }
